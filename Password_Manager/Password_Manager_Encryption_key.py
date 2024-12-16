6ee8d9e436a15a32f0949ecb499d743bd9218b9f
Khawaja Fashi Ud Din Abdullah <khawajafashi9623@gmail.com>
2024-12-17 02:15:56 +0500

    Password Manager

--- Password_Manager/Password_Manager_Encryption_key.py
+++ Password_Manager/Password_Manager_Encryption_key.py
@@ -4,11 +4,12 @@ from cryptography.fernet import Fernet
 def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)"""
+        key_file.write(key)
+"""
 
 
 def load_key():
-    file = open("key.key", "rb")
+    file = open("Password_Manager/key.key", "rb")
     key = file.read()
     file.close()
     return key
@@ -19,18 +20,18 @@ fer = Fernet(key)
 
 
 def view():
-    with open("passwords.txt", "r") as f:
+    with open("Password_Manager/passwords.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:", user, "| Password:", fer.decrypt(passw.encode()).decode())
+            print("User: ", user, "| Password: ", fer.decrypt(passw.encode()).decode())
 
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
-    with open("passwords.txt", "a") as f:
+    with open("Password_Manager/passwords.txt", "a") as f:
         f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
